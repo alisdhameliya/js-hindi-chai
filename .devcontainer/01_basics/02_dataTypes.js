1772a17f6fd07558dea217d50a83694b811303d4
alisdhameliya <108466459+alisdhameliya@users.noreply.github.com>
2023-09-20 09:08:50 +0000

    dataType done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello ");  it gives Error because alert is not a part of js , it is the part of Browser.
+
+let name = "alis"
+let age = 18
+let isLoggedIn = false
+
+// number -> 2 to power of 53 range 
+//bigInt -> not used in daily usages it important in Stock market companies, big company like redIt etc..
+// String ->""
+// boolean -> true/false  ex: loged in hai ya nahi hai , server se response aaya hai ya nahi aaya 
+// null -> standalone value 
+// undefined -> now value is not assingned 
+// symbol -> more use in react.js // to find uniqueness it is used
+
+
+console.log(typeof undefined)// interview question ,typeof undefined is undefined 
+console.log(typeof null)// type of null is  ####  object  ####
